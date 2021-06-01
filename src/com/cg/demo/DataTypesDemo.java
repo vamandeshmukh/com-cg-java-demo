58da662b3ef0bb85a8ee339573dcd5bb15f9411f
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-01 15:38:49 +0530

    Commit
    Commit

---
+++ src/com/cg/demo/DataTypesDemo.java
@@ -0,0 +1,31 @@
+package com.cg.demo;
+
+/*
+ * Data types in java 
+ * 
+ * Primitives data types - (store single value)
+ * byte short int long (integer values) (how many)
+ * float double (decimal point values / floating point values) (how much)
+ * char (character value / integer value)
+ * boolean (true / false)
+ * 
+ * Reference data types - (store multiple values)
+ * array, String, class, interface, enum, ...  
+ * 
+ */
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+
+		byte b1 = 100;
+		System.out.println(b1);
+
+	}
+
+}
+
+
+
+
+
---
+++ src/com/cg/demo/PatternDemo.java
@@ -0,0 +1,14 @@
+package com.cg.demo;
+
+public class PatternDemo {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) { // outer loop
+			for (int j = 1; j <= i; j++) { // inner loop
+				System.out.print(j);
+			}
+			System.out.println("");
+		}
+	}
+}
