ee8d7b1c6aeea7cdeb2b11968f947b34cfcb4cd4
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-01 15:53:48 +0530

    Update DataTypesDemo.java

--- src/com/cg/demo/DataTypesDemo.java
+++ src/com/cg/demo/DataTypesDemo.java
@@ -18,14 +18,22 @@ public class DataTypesDemo {
 
 	public static void main(String[] args) {
 
-		byte b1 = 100;
+		byte b1 = -128; // -128 to 127
 		System.out.println(b1);
+		int num = Integer.MAX_VALUE; // 2147483647
+		int num2 = Integer.MIN_VALUE; // -2147483648
+		System.out.println(num);
+		System.out.println(num2);
 
+		byte b2 = Byte.MAX_VALUE;
+
+		long biggestIntegerInJava = Long.MAX_VALUE; // 9223372036854775807
+		System.out.println("biggestIntegerInJava " + biggestIntegerInJava);
+
+		long phone = 9876543210L;
+		System.out.println(phone);
+
+		String PAN_CARD = "ABCDE1234E";
 	}
-
 }
-
-
-
-
 
