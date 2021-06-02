cd98dba28209a8c4d64b0105edc6f583a3243d15
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-02 12:51:02 +0530

    Commit
    Commit

--- src/com/cg/demo/oop/MethodDemo.java
+++ src/com/cg/demo/oop/MethodDemo.java
@@ -12,7 +12,7 @@ public class MethodDemo {
 		System.out.println("Some output");
 	}
 
-	static void addTwoNumbers(int num1, int num2) {
+	static void addTwoNumbers(int num1, int num2) { // parameters (variables in method signature)
 		System.out.println(num1 + num2);
 	}
 
@@ -21,11 +21,14 @@ public class MethodDemo {
 		System.out.println("Start");
 
 		someMethodName();
+//		someMethodName(10, 20);
 
-		addTwoNumbers(10, 20);
+		addTwoNumbers(10, 20); // arguments (values in method call)
 		addTwoNumbers(5, 5);
 		addTwoNumbers(15, 25);
-
+//		addTwoNumbers(10.5, 20.5);		
+//		addTwoNumbers(10, 20, 30);
+//		addTwoNumbers(10);
+//		addTwoNumbers();
 	}
-
 }
