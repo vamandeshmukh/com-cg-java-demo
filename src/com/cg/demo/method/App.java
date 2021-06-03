4a75619137a32df3ad5363a126065bb1981177fe
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-03 10:20:44 +0530

    Commit
    Commit

--- src/com/cg/demo/method/App.java
+++ src/com/cg/demo/method/App.java
@@ -6,12 +6,27 @@ public class App {
 
 		int num = 10;
 
+		// Class object = new Constructor();
 		Employee obj = new Employee();
 		obj.id = 101;
 		obj.name = "Abc";
 		obj.salary = 10.5;
 		System.out.println(obj.toString());
 
-	}
+		Employee obj2 = new Employee();
+		obj2.id = 102;
+		obj2.name = "Bbc";
+		obj2.salary = 15.25;
+		System.out.println(obj2.toString());
 
+		Employee obj3 = new Employee();
+		obj3.id = 103;
+		obj3.name = "Cbc";
+		obj3.salary = 20.75;
+		System.out.println(obj3.toString());
+
+		Employee obj4 = new Employee(104, "Ddd", 45.85);
+		System.out.println(obj4.toString());
+
+	}
 }
--- src/com/cg/demo/method/Employee.java
+++ src/com/cg/demo/method/Employee.java
@@ -1,14 +1,35 @@
 package com.cg.demo.method;
 
+/*
+ * Constructor - 
+ * It is used to create and initialize object.
+ * It is a special type of method in Java. 
+ * It does not have any return type. 
+ * Constructor name is same as class name. 
+ * By default, Java provides one default constructor.  
+ * We can create our own constructors. 
+ * If we create our constructors, Java does not provide any constructors. 
+ * 
+ * 
+ * 
+ */
+
 public class Employee {
 
 	int id;
 	String name;
 	double salary;
 
+	Employee() {
+		System.out.println("default constructor");
+	}
+
+	Employee(int id, String name, double salary) {
+		System.out.println("parameterized constructor");
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
 	}
-
 }
