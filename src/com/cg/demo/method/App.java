5aa07b4faaf2939edcdc8fd01c8a122572e120d4
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-03 09:24:38 +0530

    Commit
    Commit

---
+++ src/com/cg/demo/method/App.java
@@ -0,0 +1,17 @@
+package com.cg.demo.method;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+
+		Employee obj = new Employee();
+		obj.id = 101;
+		obj.name = "Abc";
+		obj.salary = 10.5;
+		System.out.println(obj.toString());
+
+	}
+
+}
---
+++ src/com/cg/demo/method/Employee.java
@@ -0,0 +1,14 @@
+package com.cg.demo.method;
+
+public class Employee {
+
+	int id;
+	String name;
+	double salary;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
