dde5c2fb447582c8855061dd264c707e543a3b7b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-07 15:24:58 +0530

    Commit
    Commit

--- src/com/cg/demo/ex/ExDemo.java
+++ src/com/cg/demo/ex/ExDemo.java
@@ -1,5 +1,6 @@
 package com.cg.demo.ex;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExDemo {
@@ -8,15 +9,25 @@ public class ExDemo {
 
 	public static int divide() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter 1");
-		int num1 = sc.nextInt();
-		System.out.println("Enter 2");
-		int num2 = sc.nextInt();
 		try {
+			System.out.println("Enter 1");
+			int num1 = sc.nextInt();
+			System.out.println("Enter 2");
+			int num2 = sc.nextInt();
 			return num1 / num2;
-		} catch (ArithmeticException obj) {
+		} catch (ArithmeticException ae) {
+			ae.printStackTrace();
+			System.out.println(ae);
+			System.out.println("Do not divide by zero.");
 			return 0; // business decision
+		} catch (InputMismatchException ime) {
+			System.out.println("Please enter only integer.");
+			return 0;
+		} catch (Exception e) {
+			System.out.println("Something is wrong.");
+			return 0;
 		} finally {
+			System.out.println("finally...");
 			sc.close();
 		}
 	}
