f41d1dc08d8ada8e2bfa8243def28d16e91ae2ab
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-10 15:26:54 +0530

    Update EmpDemo.java

--- src/com/cg/demo/col/EmpDemo.java
+++ src/com/cg/demo/col/EmpDemo.java
@@ -1,7 +1,12 @@
 package com.cg.demo.col;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
 
 public class EmpDemo {
 
@@ -15,10 +20,35 @@ public class EmpDemo {
 		empList.add(emp);
 		empList.add(new Employee());
 		empList.add(new Employee(103, "Aaa", 10.5));
-//		empList.add("Aaa"); // CE
+		empList.add(new Employee(104, "Bbb", 45.5));
+
+		System.out.println("Iterate using for loop");
+
+		for (int i = 0; i < empList.size(); i++) {
+			System.out.println(empList.get(i).toString());
+		}
+
+		System.out.println("Iterate using for each loop");
+
+		for (Employee e : empList) {
+			System.out.println(e.toString());
+		}
+
+		System.out.println("Iterate using Iterator");
+
+		Iterator<Employee> iterator = empList.iterator();
+
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next().toString());
+		}
+
+		// empList.add("Aaa"); // CE
 //		empList.add(delete); // CE
 
-		System.out.println(empList);
+//		Stack stack = new Stack();		
+//		Vector vector = new Vector(3, 3);
+//		System.out.println(stack.size());
+//		System.out.println(stack.capacity());
 
 	}
 
