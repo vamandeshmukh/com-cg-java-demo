34b098fe608c54d6592ebb9eb3a8b4a8695172db
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-14 10:07:57 +0530

    Update ThreadDemo.java

--- src/com/cg/demo/multi/ThreadDemo.java
+++ src/com/cg/demo/multi/ThreadDemo.java
@@ -3,15 +3,14 @@ package com.cg.demo.multi;
 public class ThreadDemo extends Thread {
 
 	public void printLoop() {
-		for (int i = 1; i <= 10; i++) {
-			try {
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println(i);
-
-		}
+//		for (int i = 1; i <= 10; i++) {
+//			try {
+//				Thread.sleep(250);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(i);
+//		}
 	}
 
 	@Override
@@ -19,12 +18,24 @@ public class ThreadDemo extends Thread {
 		this.printLoop();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ThreadDemo obj = new ThreadDemo();
 		ThreadDemo obj2 = new ThreadDemo();
 		ThreadDemo obj3 = new ThreadDemo();
 		obj.start();
+//		obj.join();
+//		System.out.println(obj.getName());
+//		obj.setName("MyThread1");
+//		System.out.println(obj.getName());
+//		System.out.println(obj2.getName());
+//		System.out.println(obj3.getName());
+		obj.setPriority(9);
+		obj2.setPriority(2);
+		obj3.setPriority(7);
+		System.out.println(obj.getPriority());
+		System.out.println(obj2.getPriority());
+		System.out.println(obj3.getPriority());
 		obj2.start();
 		obj3.start();
 //		obj.printLoop();
