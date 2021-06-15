0157d635f30372b9aad6d4b54443f868c2e0137b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-15 12:02:26 +0530

    Update App.java

--- src/com/cg/demo/reg/App.java
+++ src/com/cg/demo/reg/App.java
@@ -7,10 +7,14 @@ public class App {
 
 	public static void main(String[] args) {
 
-//		String str = "9876543210";		
+		String str = "9876543210";		
 //		System.out.println("******" + str.substring(6));
+		
+//		91**55**10
+		
+		System.out.println(str.substring(0, 2) + "**" + str.substring(4, 6));
 
-		String str = "BCD";
+//		String str = "BCD";
 		String str2 = "ABCDE";
 		Pattern pattern = Pattern.compile(str);
 		Matcher matcher = pattern.matcher(str2);
