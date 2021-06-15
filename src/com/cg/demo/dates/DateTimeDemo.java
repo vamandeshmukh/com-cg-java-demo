5a7efdad4535aad3f2176a0d24f7b5a341e77daf
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-15 16:25:56 +0530

    Update DateTimeDemo.java

--- src/com/cg/demo/dates/DateTimeDemo.java
+++ src/com/cg/demo/dates/DateTimeDemo.java
@@ -2,7 +2,12 @@ package com.cg.demo.dates;
 
 //import java.util.Date;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+
+// formatting date
+// compare two dates and find the duration
+// add / subtract duration to date
 
 public class DateTimeDemo {
 
@@ -18,14 +23,30 @@ public class DateTimeDemo {
 		System.out.println(iDay.toString());
 
 //		LocalDate myDate = LocalDate.parse("1947-08-15");
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
-		LocalDate myDate = LocalDate.parse("08-15-1947", formatter);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate myDate = LocalDate.parse("15-08-1947", formatter);
 		System.out.println(myDate.toString());
+		System.out.println(myDate.format(formatter));
+		System.out.println(LocalDate.EPOCH);
+		System.out.println(LocalDate.MIN);
+		System.out.println(LocalDate.MAX);
 
-		// formatting date
-		// compare two dates and find the duration
-		// add / subtract duration to date
+		LocalTime lt = LocalTime.now();
+		System.out.println(lt);
 
+		LocalDate manDate = LocalDate.now();
+		LocalDate expDate = manDate.plusDays(180);
+		LocalDate expDate2 = manDate.plusMonths(6);
+		System.out.println("Manufacturing date: " + manDate.toString());
+		System.out.println("Expiry date 1: " + expDate.toString());
+		System.out.println("Expiry date 2: " + expDate2.toString());
+
+		LocalDate expDate3 = manDate.minusDays(180);
+		System.out.println("Expiry date 3: " + expDate3.toString());
+
+		LocalDate date1 = LocalDate.now();
+		LocalDate date2 = LocalDate.of(2021, 12, 31);
+		System.out.println(date1.datesUntil(date2).toString());
+		System.out.println(date2.compareTo(date1));
 	}
-
 }
